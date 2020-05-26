0a51445415dc3cf979ed968344468a6e259b8c62
MarvelousMatt <mattjenkinswork@gmail.com>
2020-05-26 23:25:04 +0100

    Initial commit. Default rigidbody FPS controller with very basic shoot mechanics.

---
+++ Assets/Scripts/PlayerBullet.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBullet : MonoBehaviour
+{
+    public float speed;
+
+    private void Awake()
+    {
+        GetComponent<Rigidbody>().AddForce(transform.up * speed);
+    }
+}
---
+++ Assets/Scripts/PlayerFire.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFire : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+
+    public GameObject firePointForward;
+
+    public GameObject firePointBackward;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Fire(true);
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            Fire(false);
+        }
+    }
+
+    void Fire(bool fireForward)
+    {
+        Quaternion bulletRotation;
+        Vector3 bulletFirePos;
+
+        //Possibly cut this statement later for efficiency
+        if (fireForward)
+        {
+            bulletRotation = firePointForward.transform.rotation;
+            bulletFirePos = firePointForward.transform.position;
+        }
+        else
+        {
+            bulletRotation = firePointBackward.transform.rotation;
+            bulletFirePos = firePointBackward.transform.position;
+        }
+
+        Instantiate(bulletPrefab, bulletFirePos , bulletRotation);
+    }
+
+}
