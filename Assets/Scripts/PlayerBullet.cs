6426167dc9a4f7b67e4bc6cbf41fceb086f3e028
MarvelousMatt <mattjenkinswork@gmail.com>
2020-05-27 00:00:49 +0100

    Added explosions to bullets, adjusted player speed and handling, disabled stick to ground capabilities

--- Assets/Scripts/PlayerBullet.cs
+++ Assets/Scripts/PlayerBullet.cs
@@ -6,8 +6,32 @@ public class PlayerBullet : MonoBehaviour
 {
     public float speed;
 
+    public float explosionRadius;
+
+    public float explosionForce;
+
     private void Awake()
     {
         GetComponent<Rigidbody>().AddForce(transform.up * speed);
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Collider[] rbHits = Physics.OverlapSphere(transform.position, explosionRadius);
+
+        // Loops through the collider array and adds force to each of them.
+        //Primarily used for the player, but possibly props too?
+
+        for (int i = 0; i < rbHits.Length; i++)
+        {
+            if (rbHits[i].gameObject.GetComponent<Rigidbody>())
+                rbHits[i].attachedRigidbody.AddExplosionForce(explosionForce, transform.position, explosionRadius);
+        }
+
+        Debug.Log("Hit: " + collision.gameObject.name);
+
+        Destroy(gameObject);
+    }
+
+
 }
